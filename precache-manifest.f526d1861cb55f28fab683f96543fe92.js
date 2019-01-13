d5436ecc853a90afadcb9b66b06ac6f47153d5a8
savantrada <savantrada1022@gmail.com>
2019-01-12 22:03:18 -0500

    Updates

---
+++ precache-manifest.f526d1861cb55f28fab683f96543fe92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b294663174f5bc2478e11cfcbb3209",
+    "url": "/anotherportfolio/static/media/parul.30b29466.png"
+  },
+  {
+    "revision": "ce38a0ce151b43341b0a",
+    "url": "/anotherportfolio/static/js/runtime~main.ce38a0ce.js"
+  },
+  {
+    "revision": "698cb0022960fb4679ec",
+    "url": "/anotherportfolio/static/js/main.698cb002.chunk.js"
+  },
+  {
+    "revision": "f724cf2da0620c6293c1",
+    "url": "/anotherportfolio/static/js/1.f724cf2d.chunk.js"
+  },
+  {
+    "revision": "698cb0022960fb4679ec",
+    "url": "/anotherportfolio/static/css/main.72c3ed8d.chunk.css"
+  },
+  {
+    "revision": "1660a3b6ae90d09eab560b8c4ac52aa6",
+    "url": "/anotherportfolio/index.html"
+  }
+];
